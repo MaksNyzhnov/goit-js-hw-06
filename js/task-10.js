b4cbcf3805539f779b07268a2a76a9c0f692e282
MaksNyzhnov <123084582+MaksNyzhnov@users.noreply.github.com>
2023-05-24 22:00:51 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controlsContainerEl = document.querySelector('#controls')
+const inputEl = controlsContainerEl.firstElementChild
+const createBtnEl = inputEl.nextElementSibling
+const destroyBtnEl = controlsContainerEl.lastElementChild
+const boxContainerEl = document.querySelector('#boxes')
+
+
+createBtnEl.addEventListener('click', createBoxes)
+destroyBtnEl.addEventListener('click', destroyBoxes)
+
+
+function createBoxes(amount) {
+  amount = inputEl.value
+  const boxes = [];
+  for (let i = 1; i <= amount; i++) {
+  const boxEl = document.createElement('div')
+  boxEl.style.width = `${20 + 10 * i}px`
+  boxEl.style.height = `${20 + 10 * i}px`
+  boxEl.style.backgroundColor = `${getRandomHexColor()}`
+    boxes.push(boxEl)
+  }
+  boxContainerEl.append(...boxes)
+  console.log(boxes)
+  console.log(boxContainerEl)
+} 
+
+function destroyBoxes() {
+  boxContainerEl.innerHTML = ''
+}
